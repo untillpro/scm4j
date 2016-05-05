42d911f2d167056c05d991a0dc6a43f85b8b2025
Denis <gribanov@sigma-soft.ru>
2016-05-05 10:03:26 +0300

    get\set file content methods added IWorkspaceLocker removed

--- scm4j-vcs-api/src/main/java/com/projectkaiser/scm/vcs/api/IVCS.java
+++ scm4j-vcs-api/src/main/java/com/projectkaiser/scm/vcs/api/IVCS.java
@@ -2,8 +2,20 @@ package com.projectkaiser.scm.vcs.api;
 
 public interface IVCS {
 	void createBranch(String srcBranchPath, String dstBranchPath, String commitMessage);
+
 	PKVCSMergeResult merge(String srcBranchPath, String dstBranchPath, String commitMessage);
+
 	void deleteBranch(String branchPath, String commitMessage);
+
 	void setCredentials(String user, String password);
+
 	void setProxy(String host, int port, String proxyUser, String proxyPassword);
+
+	VCSWorkspace getWorkspace();
+
+	String getBaseUrl();
+
+	String getFileContent(String filePath);
+
+	void setFileContent(String filePath, String content, String commitMessage);
 }
