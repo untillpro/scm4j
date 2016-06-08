1816ce04b7eeaec55c3c8326d0c245edf368b1bc
Denis <gribanov@sigma-soft.ru>
2016-06-08 11:05:53 +0300

    EVCSFileNotFound exception added

---
+++ scm4j-vcs-api/src/main/java/com/projectkaiser/scm/vcs/api/exceptions/EVCSFileNotFound.java
@@ -0,0 +1,11 @@
+package com.projectkaiser.scm.vcs.api.exceptions;
+
+public class EVCSFileNotFound extends EVCSException {
+
+	private static final long serialVersionUID = 1L;
+
+	public EVCSFileNotFound(Exception e) {
+		super(e);
+	}
+
+}
