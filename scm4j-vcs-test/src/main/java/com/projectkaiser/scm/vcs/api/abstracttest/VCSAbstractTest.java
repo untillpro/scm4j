b5c67843fa973bd6aaf3f8caf93dec7aa088ec61
Denis <gribanov@sigma-soft.ru>
2016-09-05 11:21:52 +0300

    Abstract test is done

--- scm4j-vcs-test/src/main/java/com/projectkaiser/scm/vcs/api/abstracttest/VCSAbstractTest.java
+++ scm4j-vcs-test/src/main/java/com/projectkaiser/scm/vcs/api/abstracttest/VCSAbstractTest.java
@@ -1,9 +1,12 @@
 package com.projectkaiser.scm.vcs.api.abstracttest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Set;
 import java.util.UUID;
 
@@ -12,6 +15,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.mockito.exceptions.verification.WantedButNotInvoked;
 
 import com.projectkaiser.scm.vcs.api.IVCS;
 import com.projectkaiser.scm.vcs.api.PKVCSMergeResult;
@@ -20,7 +24,6 @@ import com.projectkaiser.scm.vcs.api.exceptions.EVCSFileNotFound;
 import com.projectkaiser.scm.vcs.api.workingcopy.IVCSLockedWorkingCopy;
 import com.projectkaiser.scm.vcs.api.workingcopy.IVCSRepositoryWorkspace;
 import com.projectkaiser.scm.vcs.api.workingcopy.IVCSWorkspace;
-import com.projectkaiser.scm.vcs.api.workingcopy.VCSLockedWorkingCopy;
 import com.projectkaiser.scm.vcs.api.workingcopy.VCSWorkspace;
 
 public abstract class VCSAbstractTest {
@@ -28,7 +31,7 @@ public abstract class VCSAbstractTest {
 	public static final String TEST_REPO_URL = "c:/test/utils/";
 	public static String REPO_NAME;
 	public static final String NEW_BRANCH = "new-branch";
-	public static final String SRC_BRANCH = "master";
+	public static final String MASTER_BRANCH = "master";
 	public static final String CREATED_DST_BRANCH_COMMIT_MESSAGE = "created dst branch";
 	public static final String DELETE_BRANCH_COMMIT_MESSAGE = "deleted";
 	protected static final String CONTENT_CHANGED_COMMIT_MESSAGE = "changed file content";
@@ -40,12 +43,12 @@ public abstract class VCSAbstractTest {
 	protected static final String FILE1_NAME = "test-master.txt";
 	protected static final String FILE2_NAME = "test-branch.txt";
 	protected static final String TEST_FILE_PATH = "folder/file1.txt";
+	
 	protected String repoName;
 	protected String repoUrl;
 	protected IVCSRepositoryWorkspace localVCSRepo;
 	protected IVCSWorkspace localVCSWorkspace;
 	protected IVCSRepositoryWorkspace mockedVCSRepo;
-	protected VCSLockedWorkingCopy l;
 	protected IVCSLockedWorkingCopy mockedLWC;
 	protected IVCS vcs;
 
@@ -64,7 +67,7 @@ public abstract class VCSAbstractTest {
 	}
 
 	@Before
-	public void setUp() throws IOException {
+	public void setUp() throws Exception {
 		FileUtils.deleteDirectory(new File(WORKSPACE_DIR));
 		REPO_NAME = "pk-vcs-" + getVCSTypeString() + "-testrepo";
 
@@ -77,6 +80,7 @@ public abstract class VCSAbstractTest {
 		localVCSRepo = localVCSWorkspace.getVCSRepositoryWorkspace(repoUrl);
 		mockedVCSRepo = Mockito.spy(localVCSWorkspace.getVCSRepositoryWorkspace(repoUrl));
 
+		
 		resetMocks();
 
 		createVCS(mockedVCSRepo);
@@ -84,7 +88,11 @@ public abstract class VCSAbstractTest {
 		setMakeFailureOnVCSReset(false);
 	}
 
-	protected void resetMocks() {
+	protected void resetMocks() throws Exception {
+		if (mockedLWC != null) {
+			mockedLWC.close();
+		}
+		//Mockito.reset(mockedLWC);
 		Mockito.reset(mockedVCSRepo);
 		mockedLWC = Mockito.spy(localVCSRepo.getVCSLockedWorkingCopy());
 		Mockito.doReturn(mockedLWC).when(mockedVCSRepo).getVCSLockedWorkingCopy();
@@ -92,7 +100,7 @@ public abstract class VCSAbstractTest {
 
 	@Test
 	public void testCreateAndDeleteBranch() throws Exception {
-		vcs.createBranch(SRC_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
+		vcs.createBranch(MASTER_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
 
 		verifyMocks();
 		Thread.sleep(2000); // next operation fails time to time. Looks like
@@ -102,7 +110,7 @@ public abstract class VCSAbstractTest {
 		assertTrue(getBranches().size() == 2); // master & NEW_BRANCH
 
 		try {
-			vcs.createBranch(SRC_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
+			vcs.createBranch(MASTER_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
 			fail("\"Branch exists\" situation not detected");
 		} catch (EVCSBranchExists e) {
 		}
@@ -116,20 +124,24 @@ public abstract class VCSAbstractTest {
 	}
 
 	private void verifyMocks() throws Exception {
-		Mockito.verify(mockedVCSRepo).getVCSLockedWorkingCopy();
+		try {
+			Mockito.verify(mockedVCSRepo).getVCSLockedWorkingCopy();
+		} catch (WantedButNotInvoked e) {
+			return;
+		}
 		Mockito.verify(mockedLWC).close();
 	}
 
 	@Test
 	public void testGetSetFileContent() throws Exception {
-		createTestContent(TEST_FILE_PATH, LINE_1, "master", FILE1_ADDED_COMMIT_MESSAGE);
-		vcs.setFileContent("master", TEST_FILE_PATH, LINE_2, CONTENT_CHANGED_COMMIT_MESSAGE);
+		createTestContent(TEST_FILE_PATH, LINE_1, MASTER_BRANCH, FILE1_ADDED_COMMIT_MESSAGE);
+		vcs.setFileContent(MASTER_BRANCH, TEST_FILE_PATH, LINE_2, CONTENT_CHANGED_COMMIT_MESSAGE);
 		verifyMocks();
-		assertTrue(getCommitMessagesRemote("master").contains(CONTENT_CHANGED_COMMIT_MESSAGE));
-		assertEquals(vcs.getFileContent("master", TEST_FILE_PATH), LINE_2);
-		assertEquals(vcs.getFileContent("master", TEST_FILE_PATH, "UTF-8"), LINE_2);
+		assertTrue(getCommitMessagesRemote(MASTER_BRANCH).contains(CONTENT_CHANGED_COMMIT_MESSAGE));
+		assertEquals(vcs.getFileContent(MASTER_BRANCH, TEST_FILE_PATH), LINE_2);
+		assertEquals(vcs.getFileContent(MASTER_BRANCH, TEST_FILE_PATH, "UTF-8"), LINE_2);
 		try {
-			vcs.getFileContent("master", "sdfsdf1.txt");
+			vcs.getFileContent(MASTER_BRANCH, "sdfsdf1.txt");
 			fail("EVCSFileNotFound is not thrown");
 		} catch (EVCSFileNotFound e) {
 		}
@@ -137,30 +149,30 @@ public abstract class VCSAbstractTest {
 
 	@Test
 	public void testMerge() throws Exception {
-		vcs.createBranch(SRC_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
-		createTestContent(FILE1_NAME, LINE_1, SRC_BRANCH, FILE1_ADDED_COMMIT_MESSAGE);
+		vcs.createBranch(MASTER_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
+		createTestContent(FILE1_NAME, LINE_1, MASTER_BRANCH, FILE1_ADDED_COMMIT_MESSAGE);
 		createTestContent(FILE2_NAME, LINE_2, NEW_BRANCH, FILE2_ADDED_COMMIT_MESSAGE);
 		resetMocks();
 
-		PKVCSMergeResult res = vcs.merge(NEW_BRANCH, SRC_BRANCH, MERGE_COMMIT_MESSAGE);
+		PKVCSMergeResult res = vcs.merge(NEW_BRANCH, MASTER_BRANCH, MERGE_COMMIT_MESSAGE);
 
 		verifyMocks();
 		assertFalse(mockedLWC.getCorrupted());
 		assertTrue(res.getSuccess());
 		assertTrue(res.getConflictingFiles().size() == 0);
-		String content = vcs.getFileContent(SRC_BRANCH, FILE1_NAME);
+		String content = vcs.getFileContent(MASTER_BRANCH, FILE1_NAME);
 
 		assertEquals(content, LINE_1);
-		content = vcs.getFileContent(SRC_BRANCH, FILE2_NAME);
+		content = vcs.getFileContent(MASTER_BRANCH, FILE2_NAME);
 		assertEquals(content, LINE_2);
 	}
 
 	@Test
 	public void testMergeConflict() throws Exception {
-		vcs.createBranch(SRC_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
-		createTestContent(FILE1_NAME, LINE_1, SRC_BRANCH, FILE1_ADDED_COMMIT_MESSAGE);
+		vcs.createBranch(MASTER_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
+		createTestContent(FILE1_NAME, LINE_1, MASTER_BRANCH, FILE1_ADDED_COMMIT_MESSAGE);
 		createTestContent(FILE1_NAME, LINE_2, NEW_BRANCH, FILE2_ADDED_COMMIT_MESSAGE);
-		PKVCSMergeResult res = vcs.merge(NEW_BRANCH, SRC_BRANCH, MERGE_COMMIT_MESSAGE);
+		PKVCSMergeResult res = vcs.merge(NEW_BRANCH, MASTER_BRANCH, MERGE_COMMIT_MESSAGE);
 		assertFalse(res.getSuccess());
 		assertFalse(mockedLWC.getCorrupted());
 		assertTrue(res.getConflictingFiles().size() == 1);
@@ -169,12 +181,12 @@ public abstract class VCSAbstractTest {
 
 	@Test
 	public void testMergeConflictWCCorruption() throws Exception {
-		vcs.createBranch(SRC_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
-		createTestContent(FILE1_NAME, LINE_1, SRC_BRANCH, FILE1_ADDED_COMMIT_MESSAGE);
+		vcs.createBranch(MASTER_BRANCH, NEW_BRANCH, CREATED_DST_BRANCH_COMMIT_MESSAGE);
+		createTestContent(FILE1_NAME, LINE_1, MASTER_BRANCH, FILE1_ADDED_COMMIT_MESSAGE);
 		createTestContent(FILE1_NAME, LINE_2, NEW_BRANCH, FILE2_ADDED_COMMIT_MESSAGE);
 		resetMocks();
 		setMakeFailureOnVCSReset(true);
-		PKVCSMergeResult res = vcs.merge(NEW_BRANCH, SRC_BRANCH, MERGE_COMMIT_MESSAGE);
+		PKVCSMergeResult res = vcs.merge(NEW_BRANCH, MASTER_BRANCH, MERGE_COMMIT_MESSAGE);
 		assertFalse(res.getSuccess());
 		assertTrue(mockedLWC.getCorrupted());
 		assertTrue(res.getConflictingFiles().size() == 1);
@@ -182,19 +194,36 @@ public abstract class VCSAbstractTest {
 		assertFalse(mockedLWC.getFolder().exists());
 		assertFalse(mockedLWC.getLockFile().exists());
 	}
-
+	
+	protected void createTestContent(String filePath, String fileContent, String branchName,
+			String commitMessage) throws Exception {
+		try (IVCSLockedWorkingCopy wc = localVCSRepo.getVCSLockedWorkingCopy()) {
+			checkout(branchName, wc);
+			File file = new File(wc.getFolder(), filePath);
+			file.getParentFile().mkdirs();
+			file.createNewFile();
+			PrintWriter out = new PrintWriter(file);
+			out.print(fileContent);
+			out.close();
+			
+			sendFile(wc, branchName, filePath, commitMessage);
+		}
+	}
+	
 	public abstract String getVCSTypeString();
 
 	protected abstract String getVCSRepoUrl();
 
 	protected abstract void createVCS(IVCSRepositoryWorkspace mockedVCSRepo);
 
-	protected abstract Set<String> getBranches() throws IOException;
+	protected abstract Set<String> getBranches() throws Exception;
 
 	protected abstract Set<String> getCommitMessagesRemote(String branchName) throws Exception;
 
-	protected abstract void createTestContent(String filePath, String fileContent, String branchName,
-			String commitMessage) throws Exception;
+	protected abstract void checkout(String branchName, IVCSLockedWorkingCopy wc) throws Exception;
 	
 	protected abstract void setMakeFailureOnVCSReset(Boolean doMakeFailure);
+	
+	protected abstract void sendFile(IVCSLockedWorkingCopy wc, String branchName, String filePath, String commitMessage) 
+			throws Exception;
 }
