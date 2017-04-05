c3918470e1513e8700142f05201f953df270b564
gdy <d.gribanov@dev.untill.com>
2017-04-05 10:05:23 +0300

    getCommitsRange method added

---
+++ scm4j-vcs-api/src/main/java/com/projectkaiser/scm/vcs/api/VCSCommit.java
@@ -0,0 +1,31 @@
+package com.projectkaiser.scm.vcs.api;
+
+public class VCSCommit {
+	private String id;
+	private String logMessage;
+	private String author;
+	
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getLogMessage() {
+		return logMessage;
+	}
+
+	public VCSCommit(String id, String logMessage, String author) {
+		super();
+		this.id = id;
+		this.logMessage = logMessage;
+		this.author = author;
+	}
+
+	@Override
+	public String toString() {
+		return "VCSCommit [id=" + id + ", logMessage=" + logMessage + ", author=" + author + "]";
+	}
+}
