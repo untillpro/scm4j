22ebc77d85dae3dd3d31cca5b5e19325bf6b2a4e
Denis Gribanov <d.gribanov@dev.untill.com>
2017-08-29 17:54:53 +0300

    CLI interface upgraded

--- scm4j-releaser/src/main/java/org/scm4j/wf/cli/Cli.java
+++ scm4j-releaser/src/main/java/org/scm4j/wf/cli/Cli.java
@@ -1,32 +1,53 @@
 package org.scm4j.wf.cli;
 
-import java.util.List;
-
 import org.scm4j.commons.progress.IProgress;
 import org.scm4j.commons.progress.ProgressConsole;
 import org.scm4j.wf.SCMWorkflow;
+import org.scm4j.wf.actions.ActionKind;
 import org.scm4j.wf.actions.IAction;
 import org.scm4j.wf.actions.PrintAction;
-import org.scm4j.wf.conf.Option;
+import org.scm4j.wf.exceptions.EConfig;
+import org.scm4j.wf.cli.CommandLine;
 
 public class Cli {
 
 	public static void main(String[] args) throws Exception {
-
-		String depName = args[0];
+		CommandLine cmd;
+		try {
+			cmd = new CommandLine(args);
+		} catch (EConfig e) {
+			System.out.println(e.getMessage());
+			System.out.println(CommandLine.getUsage());
+			System.exit(1);
+			return;
+			
+		}
 		
-		List<Option> options = SCMWorkflow.parseArgs(args);
-
-		
-		SCMWorkflow wf = new SCMWorkflow(options);
-		//IAction action = wf.getTagReleaseAction(depName);
-		IAction action = wf.getProductionReleaseAction(depName);
-
-		PrintAction pa = new PrintAction();
-		pa.print(System.out, action);
-
-		try (IProgress progress = new ProgressConsole(action.toString(), ">>> ", "<<< ")) {
-			action.execute(progress);
+		try {
+			SCMWorkflow wf = new SCMWorkflow(cmd.getOptions());
+			IAction action;
+			switch(cmd.getCommand()) {
+			case BUILD:
+				action = wf.getProductionReleaseAction(cmd.getProductCoords(), ActionKind.BUILD);
+				try (IProgress progress = new ProgressConsole(action.toString(), ">>> ", "<<< ")) {
+					action.execute(progress);
+				}
+				break;
+			case FORK:
+				action = wf.getProductionReleaseAction(cmd.getProductCoords(), ActionKind.FORK);
+				try (IProgress progress = new ProgressConsole(action.toString(), ">>> ", "<<< ")) {
+					action.execute(progress);
+				}
+				break;
+			case STATUS:
+				action = wf.getProductionReleaseAction(cmd.getProductCoords());
+				PrintAction pa = new PrintAction();
+				pa.print(System.out, action);
+				break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
 		}
 	}
 }
