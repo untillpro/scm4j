cb576c8ba4be0be4eb3102afbc8e84591c852c4e
kvv <kindratte@gmail.com>
2017-09-21 10:57:15 +0300

    Added coverall's and travis

--- scm4j-deployer-api/src/test/java/org/scm4j/ai/api/TravisTest.java
+++ scm4j-deployer-api/src/test/java/org/scm4j/ai/api/TravisTest.java
@@ -1,5 +1,14 @@
 package org.scm4j.ai.api;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class TravisTest {
-//
+
+    @Test
+    public void testAction() {
+        IAction act = new Action("ExeRunner");
+        assertEquals(act.getInstallerClass(), "ExeRunner");
+    }
 }
