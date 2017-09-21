f7988f9f6e00925bc592e81899447b2d9d5551a0
kvv <v.kuslin@untill.com>
2017-09-21 12:57:46 +0300

    Added unzip() from deployer-engine to save modularity, delete dep on deployer-api

--- scm4j-deployer-installers/src/main/java/org/scm4j/deployer/installers/UnzipArtifact.java
+++ scm4j-deployer-installers/src/main/java/org/scm4j/deployer/installers/UnzipArtifact.java
@@ -1,9 +1,14 @@
 package org.scm4j.deployer.installers;
 
+import lombok.Cleanup;
 import lombok.Data;
-import org.scm4j.ai.Utils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 
 @Data
@@ -17,9 +22,37 @@ public class UnzipArtifact implements IDeployer {
         this.zipFile = zipFile;
     }
 
+    public void unzip(File outputFile, File zipFile) {
+        if (!outputFile.exists()) outputFile.mkdirs();
+        byte[] buffer = new byte[1024];
+        try {
+            @Cleanup
+            FileInputStream fis = new FileInputStream(zipFile);
+            @Cleanup
+            ZipInputStream zis = new ZipInputStream(fis);
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                String fileName = ze.getName();
+                File newFile = new File(outputFile + File.separator + fileName);
+                new File(newFile.getParent()).mkdirs();
+                @Cleanup
+                FileOutputStream fos = new FileOutputStream(newFile);
+                int len;
+                while ((len = zis.read(buffer)) > 0) {
+                    fos.write(buffer, 0, len);
+                }
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+            zis.closeEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void deploy() {
-        Utils.unzip(outputFile, zipFile);
+        unzip(outputFile,zipFile);
     }
 
     @Override
