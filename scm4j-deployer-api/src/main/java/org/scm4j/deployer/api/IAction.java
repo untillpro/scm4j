48c7867b3fe5d5e0e3cd0c3bbedcd5d7ce0a7a2e
kvv <v.kuslin@untill.com>
2017-09-22 18:03:04 +0300

    Deployer's moved from deployer-installers

--- scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IAction.java
+++ scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IAction.java
@@ -3,7 +3,7 @@ package org.scm4j.deployer.api;
 import java.util.Map;
 
 public interface IAction {
-    String getInstallerClass();
+    Class getInstallerClass();
     Map<String, Object> getParams();
     Action addParam(String name, Object value);
 }
