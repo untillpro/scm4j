598e1ec64a3fd5d6e3f01dc6c3017fff8cd8ec3a
kvv <v.kuslin@untill.com>
2017-09-26 15:47:41 +0300

    Dropped IProdutStructure.getDefaultURL

--- scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IProductStructure.java
+++ scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IProductStructure.java
@@ -1,11 +1,7 @@
 package org.scm4j.deployer.api;
 
-import java.net.URL;
 import java.util.List;
 
 public interface IProductStructure {
     List<IComponent> getComponents();
-    default URL getDefaultDeploymentURL() throws Exception {
-        return new URL("C:/unTILL");
-    }
 }
