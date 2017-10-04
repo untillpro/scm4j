bb40a25343e21a752a64ea88286a500f3f90cf24
Denis Gribanov <instance6@gmail.com>
2017-10-05 01:02:24 +0300

    tests updated

--- scm4j-releaser/src/test/java/org/scm4j/releaser/conf/MDepsFileTest.java
+++ scm4j-releaser/src/test/java/org/scm4j/releaser/conf/MDepsFileTest.java
@@ -1,30 +1,62 @@
 package org.scm4j.releaser.conf;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.scm4j.releaser.TestEnvironment;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class MDepsFileTest {
-	
+
 	@Test
-	@Ignore
-	public void testMDepsFile() {
+	public void testHasMDeps() {
 		assertFalse(new MDepsFile("").hasMDeps());
 		assertFalse(new MDepsFile((String) null).hasMDeps());
 		assertFalse(new MDepsFile((List<Component>) null).hasMDeps());
-		
+		assertTrue(new MDepsFile(TestEnvironment.PRODUCT_UNTILL).hasMDeps());
+		assertTrue(new MDepsFile(Arrays.asList(new Component(TestEnvironment.PRODUCT_UNTILL)))
+				.hasMDeps());
+	}
+
+	@Test
+	public void testGetMDeps() {
 		assertTrue(new MDepsFile("").getMDeps().isEmpty());
 		assertTrue(new MDepsFile((String) null).getMDeps().isEmpty());
 		assertTrue(new MDepsFile((List<Component>) null).getMDeps().isEmpty());
-		
+		Component comp = new Component(TestEnvironment.PRODUCT_UNTILL);
+		assertTrue(new MDepsFile(TestEnvironment.PRODUCT_UNTILL).getMDeps().contains(comp));
+		assertTrue(new MDepsFile(Arrays.asList(comp)).getMDeps().contains(comp));
+	}
+
+	@Test
+	public void testToFileContent() {
 		assertTrue(new MDepsFile("").toFileContent().isEmpty());
 		assertTrue(new MDepsFile((String) null).toFileContent().isEmpty());
 		assertTrue(new MDepsFile((List<Component>) null).toFileContent().isEmpty());
-		
+		Component comp = new Component(TestEnvironment.PRODUCT_UNTILL);
+		assertTrue(new MDepsFile(TestEnvironment.PRODUCT_UNTILL).toFileContent().equals(comp.toString()));
+		assertTrue(new MDepsFile(Arrays.asList(comp)).toFileContent().equals(comp.toString()));
+	}
+
+	@Test
+	public void testReplace() {
+		Component comp = new Component(TestEnvironment.PRODUCT_UNTILL);
+		MDepsFile mdf = new MDepsFile(Arrays.asList(comp));
+		Component modifiedComp = comp.clone("11.12.13");
+		mdf.replaceMDep(modifiedComp);
+		assertTrue(mdf.getMDeps().size() == 1);
+		assertTrue(mdf.getMDeps().contains(modifiedComp));
+
+		Component wrongComp = new Component("wrong.comp:none");
+		mdf.replaceMDep(wrongComp);
+		assertTrue(mdf.getMDeps().size() == 1);
+		assertTrue(mdf.getMDeps().contains(modifiedComp));
+	}
+
+	@Test
+	public void testFormatSavingOnReplace() {
 		String content = "\r\n"
 				+ "        # my cool comment \r\n\r\n"
 				+ "    " + TestEnvironment.PRODUCT_UNTILL + " # product comment \r\n"
@@ -34,10 +66,17 @@ public class MDepsFileTest {
 		List<Component> mDeps = mdf.getMDeps();
 		assertTrue(mDeps.size() == 1);
 		assertEquals(new Component(TestEnvironment.PRODUCT_UNTILL), mDeps.get(0));
-		
+
 		Component initialComp = new Component("    " + TestEnvironment.PRODUCT_UNTILL + " # product comment ");
-		mdf.replaceMDep(initialComp.clone("12.13.14"));
-		
+		Component versionedComp = initialComp.clone("12.13.14");
+		mdf.replaceMDep(versionedComp);
+
 		assertEquals(content.replace(TestEnvironment.PRODUCT_UNTILL, TestEnvironment.PRODUCT_UNTILL + ":12.13.14"), mdf.toFileContent());
+	}
+	
+	@Test
+	public void testToString() {
+		MDepsFile mdf = new MDepsFile("");
+		assertNotNull(mdf.toString());
 	}
 }
