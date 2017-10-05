280c5373c4695387b06773c4d5153b2af2d6ae5a
kvv <v.kuslin@untill.com>
2017-10-05 18:25:14 +0300

    Executor write for untill.exe

--- scm4j-deployer-installers/src/main/java/org/scm4j/deployer/installers/Executor.java
+++ scm4j-deployer-installers/src/main/java/org/scm4j/deployer/installers/Executor.java
@@ -1,23 +1,55 @@
 package org.scm4j.deployer.installers;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 import org.scm4j.deployer.api.IComponentDeployer;
 import org.scm4j.deployer.api.IDeploymentContext;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 @Data
 public class Executor implements IComponentDeployer {
-	
-	private File product;
+
+	private static final File TMP_DIR = new File(System.getProperty("java.io.tmpdir"), "scm4j-tmp-executor");
 	private File outputDir;
+	private File product;
+	private Map<String, Object> params;
+
+	private ProcessBuilder createCmd() {
+		ProcessBuilder builder = new ProcessBuilder();
+		Set<Map.Entry<String, Object>> set = params.entrySet();
+		builder.directory(new File(TMP_DIR.getPath()));
+		List<String> cmds = new ArrayList<>();
+		cmds.add(product.getName() + ".exe");
+		set.forEach(entry -> {
+			if(entry.getValue()!= null)
+				cmds.add(entry.toString());
+			else
+				cmds.add(StringUtils.substringBefore(entry.toString(),"="));
+		});
+		builder.command(cmds);
+		return builder;
+	}
 
 	@Override
 	public void deploy() {
-		Utils.unzip(outputDir, product);
+		Utils.unzip(TMP_DIR, product);
 		try {
-			Process p = Runtime.getRuntime().exec(product.getPath());
-			p.waitFor();
+			Process p = createCmd().start();
+			StreamGobbler streamGobbler = new StreamGobbler(p.getInputStream(), System.out::println);
+			Executors.newSingleThreadExecutor().submit(streamGobbler);
+			int exitCode = p.waitFor();
+			if(exitCode != 0)
+				throw new RuntimeException("Can't install " + product.getName());
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -30,6 +62,24 @@ public class Executor implements IComponentDeployer {
 
 	@Override
 	public void init(IDeploymentContext depCtx) {
+		outputDir = new File(depCtx.getDeploymentURL().getFile());
+		params = depCtx.getParams().get(this.getClass());
+		product = depCtx.getArtifacts().get(depCtx.getMainArtifact());
+	}
 
+	private static class StreamGobbler implements Runnable {
+		private InputStream inputStream;
+		private Consumer<String> consumer;
+
+		public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+			this.inputStream = inputStream;
+			this.consumer = consumer;
+		}
+
+		@Override
+		public void run() {
+			new BufferedReader(new InputStreamReader(inputStream)).lines()
+					.forEach(consumer);
+		}
 	}
 }
