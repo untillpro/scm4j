d7c87f11822142d60395a6fde199d7ef19ebf388
kvv <v.kuslin@untill.com>
2017-10-05 11:14:23 +0300

    Added default getActionsParams() to IInastallationProcedure

--- scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IInstallationProcedure.java
+++ scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IInstallationProcedure.java
@@ -1,7 +1,12 @@
 package org.scm4j.deployer.api;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface IInstallationProcedure {
     List<IAction> getActions();
+    default Map<Class, Map<String, Object>> getActionsParams() {
+        return getActions().stream().collect(Collectors.toMap(IAction::getInstallerClass, IAction::getParams));
+    }
 }
--- scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/InstallationProcedure.java
+++ scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/InstallationProcedure.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Data
 public class InstallationProcedure implements IInstallationProcedure {
