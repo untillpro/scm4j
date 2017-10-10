9f1ef1bc5eede60a86ffa8f2617b708c6aebe7b8
kvn <v.kudryavtsev@dev.untill.com>
2017-10-10 11:37:37 +0300

    add class Settings

--- scm4j-installer/src/main/java/org/scm4j/installer/Installer.java
+++ scm4j-installer/src/main/java/org/scm4j/installer/Installer.java
@@ -1,13 +1,38 @@
 package org.scm4j.installer;
 
 import java.beans.Beans;
+import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.swt.widgets.TableItem;
+import org.scm4j.deployer.engine.DeployerEngine;
 
 public class Installer {
 
-	protected Shell shell;
+	private Settings settings;
+	private DeployerEngine deployerEngine;
+
+	protected Shell shlInstaller;
+	private Table tableProducts;
+	private Table tableVersions;
 
 	/**
 	 * Launch the application.
@@ -31,9 +56,9 @@ public class Installer {
 		if (!Beans.isDesignTime()) {
 			init();
 		}
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlInstaller.open();
+		shlInstaller.layout();
+		while (!shlInstaller.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -41,17 +66,183 @@ public class Installer {
 	}
 
 	private void init() {
+		settings = new Settings();
+
+		getProducts();
+
 		// TODO ...
+	}
+
+	private DeployerEngine getDeployerEngine() {
+		if (deployerEngine == null)
+			deployerEngine = new DeployerEngine(new File(settings.getSiteDataDir()), settings.getProductListUrl());
+		return deployerEngine;
+	}
+
+	private void getProducts() {
+		try {
+			fillProducts(getDeployerEngine().listProducts());
+		} catch (Exception e) {
+			showError("Error getting product list", e);
+		}
+	}
+
+	private void fillProducts(List<String> products) {
+		String selectedProduct = null;
+		if (tableProducts.getSelectionIndex() != -1)
+			selectedProduct = tableProducts.getItem(tableProducts.getSelectionIndex()).getText();
+		tableProducts.removeAll();
+		for (String productName: products) {
+			TableItem item = new TableItem(tableProducts, SWT.NONE);
+			item.setText(productName);
+			if (selectedProduct != null && selectedProduct.equals(productName))
+				tableProducts.setSelection(item);
+		}
+	}
+
+	private void fillVersions(Map<String, Boolean> versions, boolean refresh) {
+		String selectedVersion = null;
+		if (refresh && tableVersions.getSelectionIndex() != -1)
+			selectedVersion = tableVersions.getItem(tableVersions.getSelectionIndex()).getText();
+		tableVersions.removeAll();
+		for (String version: versions.keySet()) {
+			TableItem item = new TableItem(tableVersions, SWT.NONE);
+			item.setText(version);
+			item.setText(1, versions.get(version) ? "yes" : "no");
+			if (selectedVersion != null && selectedVersion.equals(version))
+				tableVersions.setSelection(item);
+		}
+		if (tableVersions.getSelectionIndex() == -1 && tableVersions.getItemCount() > 0)
+			tableVersions.setSelection(tableVersions.getItemCount() - 1);
+	}
+
+	private void refresh() {
+		try {
+			List<String> products = getDeployerEngine().refreshProducts();
+			fillProducts(products);
+
+			refreshVersions(true);
+		} catch (Exception e) {
+			showError("Error refreshing product list", e);
+		}
+	}
+
+	
+	private void refreshVersions(boolean refresh) {
+		try {
+			if (tableProducts.getSelectionIndex() != -1) {
+				//TableItem item = tableProducts.getItem(tableProducts.getSelectionIndex());
+				//String product = item.getText();
+				//Map<String, Boolean> versions = refresh ? getDeployerEngine().refreshProductVersions(product)
+				//		: getDeployerEngine().listProductVersions(product);
+				Map<String, Boolean> versions = new HashMap<String, Boolean>();
+				versions.put("1.1", false);
+				versions.put("2.2", true);
+				fillVersions(versions, refresh);
+			} else {
+				tableVersions.clearAll();
+			}
+		} catch (Exception e) {
+			showError("Error getting product versions", e);
+		}
+	}
+
+	private void showError(String message, final Throwable exception) {
+		MessageBox messageBox = new MessageBox(shlInstaller, SWT.ICON_ERROR | SWT.OK);
+		messageBox.setText(shlInstaller.getText());
+		if (message == null)
+			message = "";
+		if (exception != null) {
+			StringWriter sw = new StringWriter();
+			exception.printStackTrace(new PrintWriter(sw));
+			message += (message.isEmpty() ? "" : "\r\n") + sw.toString();
+		}
+		messageBox.setMessage(message);
+		messageBox.open();
 	}
 
 	/**
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWT Application");
+		shlInstaller = new Shell();
+		shlInstaller.setSize(450, 300);
+		shlInstaller.setText("Installer");
+		shlInstaller.setLayout(new FormLayout());
+		
+		SashForm sashForm = new SashForm(shlInstaller, SWT.VERTICAL);
+		FormData fd_sashForm = new FormData();
+		fd_sashForm.bottom = new FormAttachment(100, -10);
+		fd_sashForm.top = new FormAttachment(0, 10);
+		fd_sashForm.left = new FormAttachment(0, 10);
+		sashForm.setLayoutData(fd_sashForm);
+		
+		tableProducts = new Table(sashForm, SWT.BORDER | SWT.FULL_SELECTION);
+		tableProducts.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				refreshVersions(false);
+			}
+		});
+		tableProducts.setHeaderVisible(true);
+		tableProducts.setLinesVisible(true);
+		
+		TableColumn tblclmnProductName = new TableColumn(tableProducts, SWT.NONE);
+		tblclmnProductName.setWidth(300);
+		tblclmnProductName.setText("Product name");
+		
+		tableVersions = new Table(sashForm, SWT.BORDER | SWT.FULL_SELECTION);
+		tableVersions.setHeaderVisible(true);
+		tableVersions.setLinesVisible(true);
+		
+		TableColumn tblclmnVersion = new TableColumn(tableVersions, SWT.NONE);
+		tblclmnVersion.setWidth(200);
+		tblclmnVersion.setText("Version");
+		
+		TableColumn tblclmnDownloaded = new TableColumn(tableVersions, SWT.NONE);
+		tblclmnDownloaded.setWidth(100);
+		tblclmnDownloaded.setText("Downloaded");
+		sashForm.setWeights(new int[] {1, 1});
+		
+		Composite compositeButtons = new Composite(shlInstaller, SWT.NONE);
+		fd_sashForm.right = new FormAttachment(compositeButtons, -10);
+		compositeButtons.setLayout(new FormLayout());
+		FormData fd_compositeButtons = new FormData();
+		fd_compositeButtons.top = new FormAttachment(0, 10);
+		fd_compositeButtons.bottom = new FormAttachment(100, -10);
+		fd_compositeButtons.width = 100;
+		fd_compositeButtons.right = new FormAttachment(100, -10);
+		compositeButtons.setLayoutData(fd_compositeButtons);
+		
+		Button btnRefresh = new Button(compositeButtons, SWT.NONE);
+		btnRefresh.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				refresh();
+			}
+		});
+		FormData fd_btnRefresh = new FormData();
+		fd_btnRefresh.right = new FormAttachment(0, 100);
+		fd_btnRefresh.top = new FormAttachment(0);
+		fd_btnRefresh.left = new FormAttachment(0);
+		btnRefresh.setLayoutData(fd_btnRefresh);
+		btnRefresh.setText("Refresh");
+		
+		Button btnDownload = new Button(compositeButtons, SWT.NONE);
+		FormData fd_btnDownload = new FormData();
+		fd_btnDownload.right = new FormAttachment(0, 100);
+		fd_btnDownload.top = new FormAttachment(0, 31);
+		fd_btnDownload.left = new FormAttachment(0);
+		btnDownload.setLayoutData(fd_btnDownload);
+		btnDownload.setText("Download");
+		
+		Button btnInstall = new Button(compositeButtons, SWT.NONE);
+		FormData fd_btnInstall = new FormData();
+		fd_btnInstall.right = new FormAttachment(0, 100);
+		fd_btnInstall.top = new FormAttachment(0, 62);
+		fd_btnInstall.left = new FormAttachment(0);
+		btnInstall.setLayoutData(fd_btnInstall);
+		btnInstall.setText("Install");
 
 	}
-
 }
---
+++ scm4j-installer/src/main/java/org/scm4j/installer/Settings.java
@@ -0,0 +1,59 @@
+package org.scm4j.installer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Settings {
+
+	private final static String PROPERTIES_FILE_NAME = "installer.properties";
+	private final static String PROPERTY_SITE_DATA_DIR = "siteDataDir";
+	private final static String PROPERTY_PRODUCT_LIST_URL = "productListUrl";
+	private final static String DEFAULT_PRODUCT_LIST_URL = "http://scm4j.org/artifactory/repo";
+
+	private File runningPath;
+	private Properties properties;
+	private String siteDataDir;
+	private String productListUrl;
+
+	public Settings() {
+		runningPath = getRunningPath();
+
+		properties = new Properties();
+		File propertiesFile = new File(runningPath, PROPERTIES_FILE_NAME);
+		if (propertiesFile.exists()) {
+			try (InputStream inputStream = new FileInputStream(propertiesFile)) {
+				properties.load(inputStream);
+			} catch (IOException e) {
+				// TODO show warning?
+				e.printStackTrace();
+			}
+		}
+		siteDataDir = properties.getProperty(PROPERTY_SITE_DATA_DIR);
+		if (siteDataDir == null)
+			siteDataDir = new File(runningPath, "data").getPath();
+		productListUrl = properties.getProperty(PROPERTY_PRODUCT_LIST_URL, DEFAULT_PRODUCT_LIST_URL);
+	}
+
+	public String getSiteDataDir() {
+		return siteDataDir;
+	}
+
+	public String getProductListUrl() {
+		return productListUrl;
+	}
+
+	private File getRunningPath() {
+		try {
+			return new File(Installer.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath())
+					.getParentFile();
+		} catch (Exception e) {
+			// TODO show warning?
+			e.printStackTrace();
+			return new File(System.getProperty("user.dir"));
+		}
+	}
+
+}
