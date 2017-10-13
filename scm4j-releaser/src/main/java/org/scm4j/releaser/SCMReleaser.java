9f95792445cdbcc2d9ed517b7916da8d053caa07
Denis Gribanov <instance6@gmail.com>
2017-10-13 23:16:19 +0300

    refactor, optimization

--- scm4j-releaser/src/main/java/org/scm4j/releaser/SCMReleaser.java
+++ scm4j-releaser/src/main/java/org/scm4j/releaser/SCMReleaser.java
@@ -70,7 +70,7 @@ public class SCMReleaser {
 					mDeps = new DevelopBranch(comp).getMDeps();
 				}
 			}
-			mbs = getBuildStatus(calculatedStatuses, rb);
+			mbs = getBuildStatus(rb);
 			calculatedStatuses.put(comp, new CalculatedResult(rb, mbs, mDeps));
 		}
 		
@@ -93,10 +93,10 @@ public class SCMReleaser {
 		}
 	}
 
-	protected BuildStatus getBuildStatus(Map<Component, CalculatedResult> calculatedStatuses, ReleaseBranch rb) {
+	protected BuildStatus getBuildStatus(ReleaseBranch rb) {
 		BuildStatus mbs;
 		Build mb = new Build(rb);
-		mbs = mb.getStatus(calculatedStatuses);
+		mbs = mb.getStatus();
 		return mbs;
 	}
 
--- scm4j-releaser/src/test/java/org/scm4j/releaser/SCMReleaserTest.java
+++ scm4j-releaser/src/test/java/org/scm4j/releaser/SCMReleaserTest.java
@@ -1,28 +1,24 @@
 package org.scm4j.releaser;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
-
-import java.util.Map;
-
 import org.junit.Test;
 import org.scm4j.releaser.actions.ActionKind;
 import org.scm4j.releaser.actions.ActionNone;
 import org.scm4j.releaser.actions.IAction;
 import org.scm4j.releaser.branch.ReleaseBranch;
 
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
+
 public class SCMReleaserTest extends WorkflowTestBase {
 	
 	private final SCMReleaser releaser = new SCMReleaser();
 
-	@SuppressWarnings("unchecked")
 	@Test
 	public void testUnsupportedBuildStatus() {
 		SCMReleaser releaser = spy(new SCMReleaser());
-		doReturn(BuildStatus.ERROR).when(releaser).getBuildStatus(any(Map.class), any(ReleaseBranch.class));
+		doReturn(BuildStatus.ERROR).when(releaser).getBuildStatus(any(ReleaseBranch.class));
 
 		try {
 			releaser.getActionTree(TestEnvironment.PRODUCT_UNTILL, ActionKind.AUTO);
