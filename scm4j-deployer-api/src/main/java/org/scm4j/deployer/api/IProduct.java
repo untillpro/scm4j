5bf081781eb19ae635e5315c3f36a7f34da6087d
kvv <v.kuslin@untill.com>
2017-10-18 16:47:13 +0300

    Add default method in IProduct getDependentProducts()

--- scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IProduct.java
+++ scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IProduct.java
@@ -1,7 +1,11 @@
 package org.scm4j.deployer.api;
 
+import java.util.Collections;
+import java.util.List;
+
 public interface IProduct {
-
     IProductStructure getProductStructure();
-
+    default List<IProduct> getDependentProducts() {
+        return Collections.emptyList();
+    }
 }
