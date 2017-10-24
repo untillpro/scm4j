ecffa2a8265a830d0be3a1dad87ccfa800c98453
Maxim Geraskin <maxim.ge@gmail.com>
2017-10-24 14:58:30 +0300

    Update run.groovy

--- scm4j-releaser/run.groovy
+++ scm4j-releaser/run.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name='jitpack', root='https://jitpack.io')
-@Grab('com.github.scm4j:scm4j-releaser:master-SNAPSHOT')
+@Grab('com.github.scm4j:scm4j-releaser:+')
 
 import org.scm4j.releaser.cli.CLI;
 
