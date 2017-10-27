8d799b0f288270b9ce6d1b6b7975f427cdcaaf03
Denis Gribanov <d.gribanov@dev.untill.com>
2017-10-27 16:38:35 +0300

    tests updated

--- scm4j-releaser/src/test/java/org/scm4j/releaser/scmactions/SCMProcBuildTest.java
+++ scm4j-releaser/src/test/java/org/scm4j/releaser/scmactions/SCMProcBuildTest.java
@@ -1,11 +1,20 @@
 package org.scm4j.releaser.scmactions;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.scm4j.commons.progress.ProgressConsole;
+import org.scm4j.releaser.SCMReleaser;
+import org.scm4j.releaser.TestEnvironment;
 import org.scm4j.releaser.WorkflowTestBase;
+import org.scm4j.releaser.actions.ActionKind;
+import org.scm4j.releaser.actions.IAction;
 import org.scm4j.releaser.branch.ReleaseBranch;
+import org.scm4j.releaser.conf.Component;
+import org.scm4j.releaser.conf.VCSRepository;
+import org.scm4j.releaser.exceptions.ENoBuilder;
 import org.scm4j.releaser.exceptions.EReleaserException;
 
 public class SCMProcBuildTest extends WorkflowTestBase {
@@ -21,5 +30,21 @@ public class SCMProcBuildTest extends WorkflowTestBase {
 			
 		}
 	}
-
+	
+	@Test
+	public void testNoBuilder() throws Exception {
+		Component mockedComp = Mockito.spy(new Component(TestEnvironment.PRODUCT_UNTILL));
+		VCSRepository mockedRepo = Mockito.spy(mockedComp.getVcsRepository());
+		Mockito.when(mockedComp.getVcsRepository()).thenReturn(mockedRepo);
+		Mockito.when(mockedRepo.getBuilder()).thenReturn(null);
+		IAction action = new SCMReleaser().getActionTree(mockedComp, ActionKind.FORK_ONLY);
+		action.execute(new ProgressConsole());
+		ISCMProc proc = new SCMProcBuild(new ReleaseBranch(mockedComp));
+		try {
+			proc.execute(new ProgressConsole());
+			fail();
+		} catch (ENoBuilder e) {
+			assertEquals(mockedComp, e.getComp());
+		}
+	}
 }
