85f8a82c26a377153d070e1199fdb88a20541352
Denis Gribanov <d.gribanov@dev.untill.com>
2017-10-27 15:45:22 +0300

    getHeadCommit() returns null if branch is not found

--- scm4j-vcs-test/src/main/java/org/scm4j/vcs/api/abstracttest/VCSAbstractTest.java
+++ scm4j-vcs-test/src/main/java/org/scm4j/vcs/api/abstracttest/VCSAbstractTest.java
@@ -450,6 +450,8 @@ public abstract class VCSAbstractTest {
 		assertTrue(vcs.getHeadCommit(null).equals(commit2));
 		verifyMocks();
 		assertTrue(vcs.getHeadCommit(NEW_BRANCH).equals(commit3));
+		
+		assertNull(vcs.getHeadCommit("wrong branch"));
 	}
 	
 	@Test
