6366906f3a330c01e48c27fe751df869dd3f0989
Denis Gribanov <d.gribanov@dev.untill.com>
2017-11-08 11:46:58 +0300

    unnecessary BuildStatus.ERROR dropped

--- scm4j-releaser/src/main/java/org/scm4j/releaser/BuildStatus.java
+++ scm4j-releaser/src/main/java/org/scm4j/releaser/BuildStatus.java
@@ -1,5 +1,5 @@
 package org.scm4j.releaser;
 
 public enum BuildStatus {
-	FORK, FREEZE, BUILD_MDEPS, ACTUALIZE_PATCHES, BUILD, DONE, ERROR
+	FORK, FREEZE, BUILD_MDEPS, ACTUALIZE_PATCHES, BUILD, DONE
 }
--- scm4j-releaser/src/test/java/org/scm4j/releaser/SCMReleaserTest.java
+++ scm4j-releaser/src/test/java/org/scm4j/releaser/SCMReleaserTest.java
@@ -3,30 +3,11 @@ package org.scm4j.releaser;
 import org.junit.Test;
 import org.scm4j.releaser.actions.ActionKind;
 import org.scm4j.releaser.actions.IAction;
-import org.scm4j.releaser.branch.ReleaseBranch;
-
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
 
 public class SCMReleaserTest extends WorkflowTestBase {
 	
 	private final SCMReleaser releaser = new SCMReleaser();
 
-	@Test
-	public void testUnsupportedBuildStatus() throws Exception {
-		SCMReleaser releaser = spy(new SCMReleaser());
-		doReturn(BuildStatus.ERROR).when(releaser).getBuildStatus(any(ReleaseBranch.class));
-
-		try {
-			releaser.getActionTree(TestEnvironment.PRODUCT_UNTILL, ActionKind.FULL);
-			fail();
-		} catch (IllegalArgumentException e) {
-
-		}
-	}
-	
 	@Test
 	public void testGetActionTreeUsingActionKind() throws Exception {
 		IAction action = releaser.getActionTree(UNTILLDB, ActionKind.FULL);
