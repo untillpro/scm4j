f4d56f1d37fa26f41087b7fb76469d3308d9a0a6
Denis Gribanov <d.gribanov@dev.untill.com>
2017-11-24 15:31:04 +0300

    continue

--- scm4j-releaser/src/main/java/org/scm4j/releaser/ExtendedStatusTreeBuilder.java
+++ scm4j-releaser/src/main/java/org/scm4j/releaser/ExtendedStatusTreeBuilder.java
@@ -1,6 +1,8 @@
 package org.scm4j.releaser;
 
+import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.scm4j.commons.coords.Coords;
 import org.scm4j.commons.progress.IProgress;
@@ -11,12 +13,32 @@ import org.scm4j.releaser.conf.Options;
 
 public class ExtendedStatusTreeBuilder {
 	
+	private final CalculatedResult calculatedResult;
+	
+	public ExtendedStatusTreeBuilder(CalculatedResult calculatedResult) {
+		this.calculatedResult = calculatedResult;
+	}
+	
+	public Map<Coords, ExtendedStatusTreeNode> getStatusTree(Component comp) {
+		IProgress progress = new ProgressConsole();
+		Map<Coords, ExtendedStatusTreeNode> res = new ConcurrentHashMap<>();
+		
+		List<Component> mdeps;
+		Utils.async(mdeps, (mdep) -> {
+			res.
+		})
+		for (Component mdep : mdeps) {
+			
+		}
+		
+		
+	}
+	
 	public Map<Coords, ExtendedStatusTreeNode> getStatusTree(Component comp) {
 		
-		IProgress progress = new ProgressConsole();
-		
-		
-		
+		/*
+		 * получим
+		 */
 		
 		if (Options.isPatch()) {
 			ReleaseBranch rb = calculatedResult.setReleaseBranch(comp, () -> new ReleaseBranch(comp, comp.getCoords().getVersion()));
--- scm4j-releaser/src/main/java/org/scm4j/releaser/Utils.java
+++ scm4j-releaser/src/main/java/org/scm4j/releaser/Utils.java
@@ -3,6 +3,11 @@ package org.scm4j.releaser;
 import org.scm4j.commons.progress.IProgress;
 import org.scm4j.releaser.conf.Component;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ForkJoinPool;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public final class Utils {
@@ -25,5 +30,11 @@ public final class Utils {
 	}
 
 	private Utils() {
+	}
+
+	public static <T> void async(Collection<T> collection, Consumer<? super T> action) throws Exception {
+		ForkJoinPool pool = new ForkJoinPool(8);
+		pool.submit(() -> collection.parallelStream().forEach(action)).get();
+		pool.shutdown();
 	}
 }
