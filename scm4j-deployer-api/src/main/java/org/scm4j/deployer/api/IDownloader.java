224627a8a632ceaadcc084b66a6ea6c10d3cd089
kvv <v.kuslin@untill.com>
2017-12-01 16:38:17 +0300

    EIncompatibleApiVersion added

--- scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IDownloader.java
+++ scm4j-deployer-api/src/main/java/org/scm4j/deployer/api/IDownloader.java
@@ -1,5 +1,7 @@
 package org.scm4j.deployer.api;
 
+import org.scm4j.deployer.api.exceptions.EIncompatibleApiVersion;
+
 import java.io.File;
 import java.util.Map;
 
@@ -7,7 +9,7 @@ public interface IDownloader {
 
     Map<String, ? extends IDeploymentContext> getDepCtx();
 
-    File getProductFile(String coords);
+    File getProductFile(String coords) throws EIncompatibleApiVersion;
 
     IProduct getProduct();
 
