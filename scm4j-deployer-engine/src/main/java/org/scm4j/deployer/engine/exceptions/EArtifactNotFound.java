6439038c72bfd6a232c25ed32a110ae607d0c712
kvv <v.kuslin@untill.com>
2017-12-04 14:55:29 +0300

    unused exceptions deleted

--- scm4j-deployer-engine/src/main/java/org/scm4j/deployer/engine/exceptions/EArtifactNotFound.java
+++
@@ -1,9 +0,0 @@
-package org.scm4j.deployer.engine.exceptions;
-
-public class EArtifactNotFound extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public EArtifactNotFound(String message) {
-        super(message);
-    }
-}
--- scm4j-deployer-engine/src/main/java/org/scm4j/deployer/engine/exceptions/EInvalidProduct.java
+++
@@ -1,7 +0,0 @@
-package org.scm4j.deployer.engine.exceptions;
-
-public class EInvalidProduct extends RuntimeException {
-    public EInvalidProduct(String message) {
-        super(message);
-    }
-}
