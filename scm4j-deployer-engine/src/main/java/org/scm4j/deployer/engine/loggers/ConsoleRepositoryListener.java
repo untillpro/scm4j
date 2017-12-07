dfa05094ad36c4df350aef7193efbf5ad9e0ff60
kvv <v.kuslin@untill.com>
2017-12-07 17:12:44 +0300

    Unused console repository listener method's deleted

--- scm4j-deployer-engine/src/main/java/org/scm4j/deployer/engine/loggers/ConsoleRepositoryListener.java
+++ scm4j-deployer-engine/src/main/java/org/scm4j/deployer/engine/loggers/ConsoleRepositoryListener.java
@@ -8,23 +8,6 @@ import org.eclipse.aether.RepositoryEvent;
 public class ConsoleRepositoryListener
         extends AbstractRepositoryListener {
 
-    public void artifactDeployed(RepositoryEvent event) {
-        log.info("Deployed " + event.getArtifact() + " to " + event.getRepository());
-    }
-
-    public void artifactDeploying(RepositoryEvent event) {
-        log.info("Deploying " + event.getArtifact() + " to " + event.getRepository());
-    }
-
-    public void artifactDescriptorInvalid(RepositoryEvent event) {
-        log.info("Invalid artifact descriptor for " + event.getArtifact() + ": "
-                + event.getException().getMessage());
-    }
-
-    public void artifactDescriptorMissing(RepositoryEvent event) {
-        log.info("Missing artifact descriptor for " + event.getArtifact());
-    }
-
     public void artifactInstalled(RepositoryEvent event) {
         log.info("Installed " + event.getArtifact() + " to " + event.getFile());
     }
@@ -49,32 +32,12 @@ public class ConsoleRepositoryListener
         log.trace("Resolving artifact " + event.getArtifact());
     }
 
-    public void metadataDeployed(RepositoryEvent event) {
-        log.info("Deployed " + event.getMetadata() + " to " + event.getRepository());
-    }
-
-    public void metadataDeploying(RepositoryEvent event) {
-        log.info("Deploying " + event.getMetadata() + " to " + event.getRepository());
-    }
-
     public void metadataInstalled(RepositoryEvent event) {
         log.info("Installed " + event.getMetadata() + " to " + event.getFile());
     }
 
     public void metadataInstalling(RepositoryEvent event) {
         log.info("Installing " + event.getMetadata() + " to " + event.getFile());
-    }
-
-    public void metadataInvalid(RepositoryEvent event) {
-        log.trace("Invalid metadata " + event.getMetadata());
-    }
-
-    public void metadataResolved(RepositoryEvent event) {
-        log.info("Resolved metadata " + event.getMetadata() + " from " + event.getRepository());
-    }
-
-    public void metadataResolving(RepositoryEvent event) {
-        log.info("Resolving metadata " + event.getMetadata() + " from " + event.getRepository());
     }
 
 }
