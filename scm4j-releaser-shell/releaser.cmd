725f9ab83ed57a23a49fc19a76fa6334f4f18fa0
maxim-ge <maxim.ge@gmail.com>
2017-12-27 11:22:47 +0300

    Single cmd

--- scm4j-releaser-shell/releaser.cmd
+++ scm4j-releaser-shell/releaser.cmd
@@ -1,2 +1,40 @@
 @echo off
-call %~dp0releaser-git git.exe sh.exe %*
+if [%1] == [git.exe] goto normal
+if [%1] == [java.exe] goto test_empty_path
+
+call %~dp0releaser git.exe sh.exe %*
+exit
+
+:test_empty_path
+SET PATH=%~dp$PATH:1
+echo Test path=%PATH%
+./releaser.cmd %2 %3 %4 %5 %6
+exit
+
+:normal
+@echo off
+set gitpath=%~dp$PATH:1
+if ."%gitpath%" == ."" goto try_git
+goto try_sh
+
+:try_git
+PATH=%PATH%;c:/Program Files/Git/Cmd
+set gitpath=%~dp$PATH:1
+if ."%gitpath%" == ."" goto git_not_found
+
+:try_sh
+PATH=%PATH%;%gitpath%../usr/bin
+set shpath=%~dp$PATH:2
+if ."%shpath%" == ."" goto sh_not_found
+
+"%shpath%sh" %~dp0releaser %3 %4 %5 %6 %7 %8 %9
+
+exit
+
+:git_not_found
+echo %1 not found
+exit 1
+
+:sh_not_found
+echo %2 not found
+exit 1
