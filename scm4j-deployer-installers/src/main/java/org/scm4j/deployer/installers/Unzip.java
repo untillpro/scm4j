c76a73c9d37b369425b7aabcc87767184123e021
kvv <v.kuslin@untill.com>
2018-01-09 16:32:56 +0300

    #ver 0.3.0

--- scm4j-deployer-installers/src/main/java/org/scm4j/deployer/installers/Unzip.java
+++ scm4j-deployer-installers/src/main/java/org/scm4j/deployer/installers/Unzip.java
@@ -3,48 +3,43 @@ package org.scm4j.deployer.installers;
 import lombok.Cleanup;
 import lombok.Data;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.scm4j.deployer.api.DeploymentResult;
 import org.scm4j.deployer.api.IComponentDeployer;
 import org.scm4j.deployer.api.IDeploymentContext;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.util.Enumeration;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import java.util.zip.ZipFile;
 
 
 @Data
 public class Unzip implements IComponentDeployer {
 
-    private File outputFile;
-    private File zipFile;
+    private File outputDir;
+    private File zipFileName;
 
     @Override
     public DeploymentResult deploy() {
-        if (!outputFile.exists()) outputFile.mkdirs();
-        byte[] buffer = new byte[1024];
         try {
             @Cleanup
-            FileInputStream fis = new FileInputStream(zipFile);
-            @Cleanup
-            ZipInputStream zis = new ZipInputStream(fis);
-            ZipEntry ze = zis.getNextEntry();
-            while (ze != null) {
-                String fileName = ze.getName();
-                File newFile = new File(outputFile + File.separator + fileName);
-                new File(newFile.getParent()).mkdirs();
-                @Cleanup
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                    fos.write(buffer, 0, len);
+            ZipFile zipFile = new ZipFile(zipFileName);
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                File entryDestination = new File(outputDir, entry.getName());
+                if (entry.isDirectory()) {
+                    entryDestination.mkdirs();
+                } else {
+                    entryDestination.getParentFile().mkdirs();
+                    @Cleanup
+                    InputStream in = zipFile.getInputStream(entry);
+                    @Cleanup
+                    OutputStream out = new FileOutputStream(entryDestination);
+                    IOUtils.copy(in, out);
                 }
-                zis.closeEntry();
-                ze = zis.getNextEntry();
             }
-            zis.closeEntry();
             return DeploymentResult.OK;
         } catch (IOException e) {
             return DeploymentResult.NEED_REBOOT;
@@ -55,19 +50,14 @@ public class Unzip implements IComponentDeployer {
     public DeploymentResult undeploy() {
         try {
             @Cleanup
-            FileInputStream fis = new FileInputStream(zipFile);
-            @Cleanup
-            ZipInputStream zis = new ZipInputStream(fis);
-            ZipEntry ze = zis.getNextEntry();
-            while (ze != null) {
-                String fileName = ze.getName();
-                File newFile = new File(outputFile + File.separator + fileName);
-                if (newFile.exists())
-                    FileUtils.forceDelete(newFile);
-                zis.closeEntry();
-                ze = zis.getNextEntry();
+            ZipFile zipFile = new ZipFile(zipFileName);
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                ZipEntry entry = entries.nextElement();
+                File entryDestination = new File(outputDir, entry.getName());
+                if (entryDestination.exists())
+                    FileUtils.forceDelete(entryDestination);
             }
-            zis.closeEntry();
             return DeploymentResult.OK;
         } catch (IOException e) {
             return DeploymentResult.NEED_REBOOT;
@@ -86,7 +76,11 @@ public class Unzip implements IComponentDeployer {
 
     @Override
     public void init(IDeploymentContext depCtx) {
-        outputFile = new File(depCtx.getDeploymentPath());
-        zipFile = depCtx.getArtifacts().get(depCtx.getMainArtifact());
+        this.outputDir = new File(depCtx.getDeploymentPath());
+        this.zipFileName = depCtx.getArtifacts().get(depCtx.getMainArtifact());
+        try (ZipFile zipFile = new ZipFile(this.zipFileName)) {
+        } catch (IOException e) {
+            throw new RuntimeException("Not a zip file!", e);
+        }
     }
 }
--- scm4j-deployer-installers/src/test/java/org/scm4j/deployer/installers/UnzipTest.java
+++ scm4j-deployer-installers/src/test/java/org/scm4j/deployer/installers/UnzipTest.java
@@ -1,8 +1,9 @@
 package org.scm4j.deployer.installers;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.scm4j.deployer.api.DeploymentContext;
 import org.scm4j.deployer.api.DeploymentResult;
@@ -20,58 +21,99 @@ public class UnzipTest {
 
     private static final File ZIP_FOLDER = new File(System.getProperty("java.io.tmpdir"), "test-zip");
     private static final File ZIP_FILE = new File(ZIP_FOLDER, "file.zip");
+    private static final String ZIPPED_FOLDER_NAME = "zipped-folder/";
     private DeploymentContext depCtx;
 
-    @Before
-    public void setUp() throws Exception {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
         ZIP_FOLDER.mkdirs();
         ZIP_FILE.createNewFile();
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(ZIP_FILE));
+        ZipEntry entry;
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0)
+                entry = new ZipEntry(ZIPPED_FOLDER_NAME + "mytext" + i + ".txt");
+            else
+                entry = new ZipEntry("mytext" + i + ".txt");
+            String str = "Test String" + i;
+            out.putNextEntry(entry);
+            byte[] data = str.getBytes();
+            out.write(data, 0, data.length);
+            out.closeEntry();
+        }
+        out.close();
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        FileUtils.deleteDirectory(ZIP_FOLDER);
+    }
+
+    @Before
+    public void before() {
         depCtx = new DeploymentContext(ZIP_FILE.getName());
         depCtx.setDeploymentPath(ZIP_FOLDER.getPath());
         Map<String, File> artifacts = new HashMap<>();
         artifacts.put(ZIP_FILE.getName(), ZIP_FILE);
         depCtx.setArtifacts(artifacts);
-        String str = "Test String";
-        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(ZIP_FILE));
-        ZipEntry entry = new ZipEntry("mytext.txt");
-        out.putNextEntry(entry);
-        byte[] data = str.getBytes();
-        out.write(data, 0, data.length);
-        out.closeEntry();
-        out.close();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        FileUtils.deleteDirectory(ZIP_FOLDER);
     }
 
     @Test
     public void testDeployUndeploy() throws Exception {
         Unzip unzip = new Unzip();
-        File textFile = new File(ZIP_FOLDER, "mytext.txt");
         unzip.init(depCtx);
+
         unzip.deploy();
-        assertEquals(FileUtils.readFileToString(textFile, "UTF-8"), "Test String");
+        File testFile;
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0)
+                testFile = new File(ZIP_FOLDER, ZIPPED_FOLDER_NAME + "mytext" + i + ".txt");
+            else
+                testFile = new File(ZIP_FOLDER, "mytext" + i + ".txt");
+            assertEquals(FileUtils.readFileToString(testFile, "UTF-8"), "Test String" + i);
+        }
+
         unzip.undeploy();
-        assertFalse(textFile.exists());
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0)
+                testFile = new File(ZIP_FOLDER, ZIPPED_FOLDER_NAME + "mytext" + i + ".txt");
+            else
+                testFile = new File(ZIP_FOLDER, "mytext" + i + ".txt");
+            assertFalse(testFile.exists());
+        }
     }
 
     @Test
     public void testFail() throws Exception {
         Unzip unzip = new Unzip();
         unzip.init(depCtx);
-        FileUtils.forceDelete(unzip.getZipFile());
+
+        FileUtils.forceDelete(unzip.getZipFileName());
         DeploymentResult res = unzip.deploy();
         assertNotEquals(DeploymentResult.OK, res);
+
+        beforeClass();
+    }
+
+    @Test
+    public void testNotZippedDirectory() {
+        Unzip unzip = new Unzip();
+        Map<String, File> arts = new HashMap<>();
+        arts.put(ZIP_FILE.getName(), new File("C:/"));
+        depCtx.setArtifacts(arts);
+        try {
+            unzip.init(depCtx);
+            fail();
+        } catch (RuntimeException e) {
+        }
     }
 
     @Test
     public void testInit() throws Exception {
         Unzip unzip = new Unzip();
         unzip.init(depCtx);
-        assertEquals(unzip.getZipFile(), depCtx.getArtifacts().get(ZIP_FILE.getName()));
-        assertTrue(FileUtils.contentEquals(unzip.getZipFile(), depCtx.getArtifacts().get(ZIP_FILE.getName())));
-        assertEquals(unzip.getOutputFile(), new File(depCtx.getDeploymentPath()));
+        assertEquals(unzip.getZipFileName(), depCtx.getArtifacts().get(ZIP_FILE.getName()));
+        assertTrue(FileUtils.contentEquals(unzip.getZipFileName(), depCtx.getArtifacts().get(ZIP_FILE.getName())));
+        assertEquals(unzip.getOutputDir(), new File(depCtx.getDeploymentPath()));
     }
 }
