434f64ecfc347aaaf9dd87775664b897d4e12c7c
Denis Gribanov <d.gribanov@dev.untill.com>
2018-01-17 10:05:20 +0300

    tests update

--- scm4j-releaser/src/test/java/org/scm4j/releaser/builders/CmdLineBuilderTest.java
+++ scm4j-releaser/src/test/java/org/scm4j/releaser/builders/CmdLineBuilderTest.java
@@ -2,6 +2,7 @@ package org.scm4j.releaser.builders;
 
 import org.apache.commons.io.input.ReaderInputStream;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.scm4j.commons.progress.IProgress;
@@ -29,31 +30,40 @@ public class CmdLineBuilderTest {
 	private static final List<String> TEST_CMD_LINE_LIST = Arrays.asList("cmd.exe", "/C", "\"sdsds ddgfgf\"",
 			"gradlew.bat", "build");
 	private static final String TEST_PROCESS_ERROR = "test process error";
-	public static final String TEST_ENV_VAR_NAME = "envVar";
-	public static final String TEST_ENV_VAR_VALUE = "value";
+	private static final String TEST_ENV_VAR_NAME = "envVar";
+	private static final String TEST_ENV_VAR_VALUE = "value";
+	
+	private CmdLineBuilder clb;
+	private Component comp;
+	private File workingFolder;
+	private IProgress mockedProgress;
+	private Map<String, String> envVars;
+	private Process mockedProc;
+	
+	@Before
+	public void setUp() throws Exception {
+		clb = spy(new CmdLineBuilder(TEST_CMD_LINE));
+		comp = new Component(TestEnvironment.PRODUCT_UNTILLDB);
+		workingFolder = new File(TestEnvironment.TEST_REMOTE_REPO_DIR); 
+		envVars = new HashMap<>();
+		envVars.put(TEST_ENV_VAR_NAME, TEST_ENV_VAR_VALUE);
+		
+		mockedProc = mock(Process.class);
+		mockedProgress = mock(IProgress.class);
+
+		doReturn(mockedProc).when(clb).getProcess(any(ProcessBuilder.class));
+		
+		StringReader procOutputReader = new StringReader(TEST_PROCESS_OUTPUT);
+		InputStream processOutputStream = new ReaderInputStream(procOutputReader, StandardCharsets.UTF_8);
+		doReturn(processOutputStream).when(mockedProc).getInputStream();
+		
+		StringReader procErrorReader = new StringReader(TEST_PROCESS_ERROR);
+		InputStream processErrorStream = new ReaderInputStream(procErrorReader, StandardCharsets.UTF_8);
+		doReturn(processErrorStream).when(mockedProc).getErrorStream();
+	}
 
 	@Test
 	public void testBuild() throws Exception {
-		CmdLineBuilder clb = spy(new CmdLineBuilder(TEST_CMD_LINE));
-		Component comp;
-		try (TestEnvironment env = new TestEnvironment()) {
-			env.generateTestEnvironmentNoVCS();
-			comp = new Component(TestEnvironment.PRODUCT_UNTILLDB);
-		}
-
-		File workingFolder = new File(TestEnvironment.TEST_REMOTE_REPO_DIR); 
-		StringReader procOutputReader = new StringReader(TEST_PROCESS_OUTPUT);
-		InputStream processOutputStream = new ReaderInputStream(procOutputReader, StandardCharsets.UTF_8);
-		StringReader procErrorReader = new StringReader(TEST_PROCESS_ERROR);
-		InputStream processErrorStream = new ReaderInputStream(procErrorReader, StandardCharsets.UTF_8);
-		Map<String, String> envVars = new HashMap<>();
-		envVars.put(TEST_ENV_VAR_NAME, TEST_ENV_VAR_VALUE);
-		
-		Process mockedProc = mock(Process.class);
-		IProgress mockedProgress = mock(IProgress.class);
-		doReturn(mockedProc).when(clb).getProcess(any(ProcessBuilder.class));
-		doReturn(processOutputStream).when(mockedProc).getInputStream();
-		doReturn(processErrorStream).when(mockedProc).getErrorStream();
 		clb.build(comp, workingFolder, mockedProgress, envVars);
 
 		verify(mockedProgress).reportStatus(TEST_PROCESS_OUTPUT);
@@ -66,7 +76,10 @@ public class CmdLineBuilderTest {
 		assertEquals(TEST_ENV_VAR_VALUE, pbToTest.environment().get(TEST_ENV_VAR_NAME));
 		assertTrue(TEST_CMD_LINE_LIST.containsAll(pbToTest.command()));
 		assertTrue(pbToTest.command().containsAll(TEST_CMD_LINE_LIST));
-		
+	}
+	
+	@Test
+	public void testBuildFailure() throws Exception {
 		doReturn(1).when(mockedProc).waitFor();
 		try {
 			clb.build(comp, workingFolder, mockedProgress, envVars);
