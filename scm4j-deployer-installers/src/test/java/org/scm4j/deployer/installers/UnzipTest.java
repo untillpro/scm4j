a18ca3a63e1abb0981fdad8ee6a7bc661babf50d
kvv <v.kuslin@untill.com>
2018-10-26 15:16:22 +0300

    Methods for work with JSON refactored, Map's replaced with objects, DeployerEngine returns Map<String, ProductInfo> instead of List<String> products

--- scm4j-deployer-installers/src/test/java/org/scm4j/deployer/installers/UnzipTest.java
+++ scm4j-deployer-installers/src/test/java/org/scm4j/deployer/installers/UnzipTest.java
@@ -15,7 +15,11 @@ import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class UnzipTest {
 
@@ -105,6 +109,7 @@ public class UnzipTest {
 			unzip.init(depCtx);
 			fail();
 		} catch (RuntimeException e) {
+			//
 		}
 	}
 
